1833d6466393989facaf46f55834e913f96b9a2f
cthwaite <thwaite@mac.com>
2018-07-09 21:39:28 +0100

    Add <Container> component

---
+++ src/components/Container.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import classNames from 'classnames';
+
+import { mapClassNames } from './classMaps';
+
+
+export default class Container extends Component {
+    render() {
+        const {
+            children,
+            className,
+        } = this.props;
+
+        const classes = classNames(
+            mapClassNames(this.props),
+            className
+        );
+        return (
+            <div className={classes}>{children}</div>
+        );
+    }
+}
