5570c927b940b7c47adecf26d3e7d3729e6890b2
cthwaite <thwaite@mac.com>
2018-07-09 21:16:34 +0100

    Add class mapper for textAlign and vertAlign

---
+++ src/components/classMaps/aligns.js
@@ -0,0 +1,45 @@
+import PropTypes from 'prop-types';
+
+const textAlignClass = ({textAlign}, bpSuffix='') => {
+    switch(textAlign) {
+        case 'left':
+            return `tl${bpSuffix}`;
+        case 'right':
+            return `tr${bpSuffix}`;
+        case 'center':
+            return `tc${bpSuffix}`;
+        case 'justify':
+            return `tj${bpSuffix}`;
+        default:
+            return;
+    }
+};
+
+const textAlignPropType = PropTypes.oneOf([
+    'left', 'right', 'center', 'justify'
+]);
+
+
+const vertAlignClass = ({vertAlign}, bpSuffix='') => {
+    switch(textAlign) {
+        case 'base':
+            return `v-base${bpSuffix}`;
+        case 'mid':
+            return `v-mid${bpSuffix}`;
+        case 'top':
+            return `v-top${bpSuffix}`;
+        case 'bottom':
+            return `v-btm${bpSuffix}`;
+        default:
+            return;
+    }
+};
+
+const vertAlignPropType = PropTypes.oneOf([
+    'base', 'mid', 'top', 'bottom'
+]);
+
+export {
+    textAlignClass, textAlignPropType,
+    vertAlignClass, vertAlignPropType
+};
