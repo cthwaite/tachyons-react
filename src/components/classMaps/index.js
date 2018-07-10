cedc161ff89082d53af465183786e1451750089c
cthwaite <thwaite@mac.com>
2018-07-10 13:32:52 +0100

    Repair functionality of classMaps/index.js

--- src/components/classMaps/index.js
+++ src/components/classMaps/index.js
@@ -1,3 +1,5 @@
+import classNames from 'classnames';
+
 import { borderClass } from './borders';
 import { clearFixClass, clearClass } from './clear';
 import { colorClass } from './colors';
@@ -16,25 +18,25 @@ import { partition } from './utility';
 
 /// Build a mapper from props to tachyons classes.
 const buildPropToClassMap = propMap => {
-    const newMapper = {};
-    for(const key of Object.keys(propMap)) {
-        newMapper[key] = propMap[key];
-    }
     const mapToClasses = (obj, breakpointSuffix) => (
         Object.entries(obj)
             .map(([key, value]) => newMapper[key](value, breakpointSuffix))
     );
-    /// Recursively map breakpoint properties.
-    newMapper['notSmall'] = value => mapToClasses(value, '-ns');
+    const newMapper = {};
+    for(const key of Object.keys(propMap)) {
+        newMapper[key] = propMap[key];
+    }
+    /// Recursively map breakpoint properties. 'value' here is an object.
+    newMapper['notSmall'] = value =>  mapToClasses(value, '-ns'),
     newMapper['medium'] = value => mapToClasses(value, '-m');
     newMapper['large'] = value => mapToClasses(value, '-l');
 
-    return context => {
-        if(!context) {
+    return props => {
+        if(!props) {
             return;
         }
-
-        const [have, haveNot] = partition(context, newMapper);
+        // Split out the props into those matching tachyons keys, and everything else.
+        const [have, haveNot] = partition(props, newMapper);
         return [mapToClasses(have), haveNot];
     };
 };
@@ -65,12 +67,20 @@ const PROP_TO_CLASS_DEFAULTS = {
 };
 
 /// Default mapping between props and tachyons classes, using PROP_TO_CLASS_DEFAULTS.
-const mapClassNames = buildPropToClassMap(PROP_TO_CLASS_DEFAULTS);
-
+const mapAllClassNames = buildPropToClassMap(PROP_TO_CLASS_DEFAULTS);
 const DEFAULT_PROP_KEYS = Array.from(Object.keys(PROP_TO_CLASS_DEFAULTS));
-export const splitProps = props => partition(props, DEFAULT_PROP_KEYS);
+
+/// Return a function transforming the passed 'props' into a string of classes and a list of restprops.
+const partitionToClasses = classMapper => props => {
+    const { className } = props;
+    const [derivedClasses, restProps] = classMapper(props);
+    return [classNames(derivedClasses, className), restProps];
+};
+
+const defaultPartition = partitionToClasses(mapAllClassNames);
 
 export {
     buildPropToClassMap,
-    mapClassNames
+    mapAllClassNames,
+    defaultPartition
 };
