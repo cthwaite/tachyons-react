150bc3b3127c1ded02b189eaa5400e9b5b6d25e4
cthwaite <thwaite@mac.com>
2018-07-10 22:10:37 +0100

    Update typography story to use new utility classes

--- src/stories/typography.js
+++ src/stories/typography.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
+import { bakeClasses } from '../utility';
+
 import Code from '../components/Code';
 import Container from '../components/Container';
 import Heading from '../components/Heading';
 import Paragraph from '../components/Paragraph';
 
+
+const DescHead = bakeClasses(Heading, {
+    f:6,
+    fw:6,
+    pt:5,
+    mt:2,
+    mb:0,
+    tracked: true,
+    textTransform: 'uppercase',
+    border:'top',
+});
+
+const OuterWrap = bakeClasses(Container, {
+    pb:5,
+    ph:5,
+    mt:3,
+    fontFamily: 'sans-serif',
+    notSmall: { ph: 5, mb: 5, pb: 5, pt: 0 }
+});
+
 export const typographyStory = storiesOf('typography', module);
 typographyStory
     .add('Heading', () => (
-        <Container pb={5} ph={5} mt={3} notSmall={{ ph: 5, mb: 5, pb: 5, pt: 0 }}>
-        <Container color='black-70' fontFamily='sans-serif' maxWidth={9}>
+        <OuterWrap>
             <Code mb={3} f={6} mb={3} display='block'>{`<Heading fontSize={1} lineHeight='title'>`}</Code>
             <Heading fontSize={1} lineHeight='title'>A modular scale, like a musical scale, is a prearranged set of harmonious proportions.</Heading>
             <Code mb={3} f={6} mb={3} display='block'>{`<Heading f={2} lineHeight='copy'>`}</Code>
@@ -21,8 +42,7 @@ typographyStory
             <Heading f={4} lineHeight='copy'>A modular scale, like a musical scale, is a prearranged set of harmonious proportions.</Heading>
             <Code mb={3} f={6} mb={3} display='block'>{`<Heading f={5} lineHeight='copy'>`}</Code>
             <Heading f={5} lineHeight='copy'>A modular scale, like a musical scale, is a prearranged set of harmonious proportions.</Heading>
-        </Container>
-        </Container>
+        </OuterWrap>
     ));
 typographyStory.add('Headline', () => (
     <Container pb={5} ph={5} mt={3} notSmall={{ ph: 5, mb: 5, pb: 5, pt: 0 }}>
@@ -58,25 +78,26 @@ typographyStory
                 </Paragraph>
             </Container>
         </Container>
-    ));
+));
+
 
 typographyStory
     .add('Measure', () => (
-        <Container pb={5} ph={5} mt={3} notSmall={{ ph: 5, mb: 5, pb: 5, pt: 0 }}>
+        <Container padding={{ pb: 5, ph: 5 }} mt={3} notSmall={{ ph: 5, mb: 5, pb: 5, pt: 0 }}>
         <Container color='black-70' fontFamily='sans-serif' maxWidth={9}>
-            <Heading f={6} fw={6} pt={5} mt={2} mb={0} tracked textTransform='uppercase' border='top'>5rem/80px at 30em</Heading>
+            <DescHead>5rem/80px at 30em</DescHead>
             <Code f={6} mt={3} display='block'>{`<Heading tag='h3' subheadline measure>`}</Code>
             <Heading tag='h3' mt={4} subheadline measure>
                 Designers create hierarchy and contrast by playing with the scale of letterforms.
             </Heading>
 
-            <Heading f={6} fw={6} pt={5} mt={2} mb={0} tracked textTransform='uppercase' border='top'>3rem(48px) at 30em</Heading>
+            <DescHead>3rem(48px) at 30em</DescHead>
             <Code f={6} mt={3} display='block'>{`<Heading tag='h3' f={1} measure>`}</Code>
             <Heading tag='h3' f={1} mt={4} measure lineHeight='title'>
             Designers create hierarchy and contrast by playing with the scale of letterforms. Changes in scale help create visual contrast, movement, and depth as well as express hierarchies of importance.
             </Heading>
 
-            <Heading f={6} fw={6} pt={5} mt={2} mb={0} tracked textTransform='uppercase' border='top'>2.25rem (36px) at 30em</Heading>
+            <DescHead>2.25rem (36px) at 30em</DescHead>
             <Code f={6} mt={3} display='block'>{`<Heading tag='h3' f={2} measure>`}</Code>
             <Heading tag='h3' f={2} measure lineHeight='copy'>
             Designers create hierarchy and contrast by playing with the scale of letterforms. Changes in scale help create visual contrast, movement, and depth as well as express hierarchies of importance.
